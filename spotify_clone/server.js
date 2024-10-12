aa2fd2c90a7a28299e0578dbc32c29cd7eb50783
Muninn1137 <152490966+Muninn1137@users.noreply.github.com>
2024-10-12 20:19:47 +0200

    es spotify clone pt2

--- spotify_clone/server.js
+++ spotify_clone/server.js
@@ -1,1 +1,11 @@
-alert("hello world")
+window.addEventListener("load", async () => {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization:
+          "Bearer BQA1k0PdQqNceYZheVMe8krjl_YHlc4xO3JD5DAacpZwxMIWapF9G08W7WvM9j-FYrlx3bsKyaGGvbmABMkZYIiBE7t_uAiz4LjtfZIyR3TUXM4k_bM",
+      },
+    });
+  
+    console.log(result);
+  });
+  
